23a0ad0e304a83343d15e729f5cc3347f4f5719d
wmqKevin <33646381+wmqKevin@users.noreply.github.com>
2026-07-09 01:33:03 +0800

    release: v0.8 (ZI-111)

--- src/app/GameApp.ts
+++ src/app/GameApp.ts
@@ -1,10 +1,17 @@
 import Phaser from 'phaser';
 import { createPhaserConfig } from './phaserConfig';
 
+declare global {
+  interface Window {
+    __ROGUELIKE_TETRIS_GAME__?: Phaser.Game;
+  }
+}
+
 let game: Phaser.Game | undefined;
 
 export function startGame(parent: string): Phaser.Game {
   game?.destroy(true);
   game = new Phaser.Game(createPhaserConfig(parent));
+  window.__ROGUELIKE_TETRIS_GAME__ = game;
   return game;
 }
